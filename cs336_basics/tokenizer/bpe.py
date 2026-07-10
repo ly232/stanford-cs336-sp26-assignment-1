fbd1139d3f1d649f6f5474a53798042d85f79651
Lin Yang <yang7.cooper@gmail.com>
2026-07-10 16:21:21 +0800

    first iteration of bpe encoding algorithm, with unit tests replicating the original bpe paper test case.

--- cs336_basics/tokenizer/bpe.py
+++ cs336_basics/tokenizer/bpe.py
@@ -25,15 +25,71 @@ class BytePairEncoder:
         for sp in special_tokens:
             self._vocabs[len(self._vocabs)] = sp.encode()
         self._merges: list[tuple[bytes, bytes]] = []
-        self._freq: dict[tuple[bytes, bytes], int] \
+        # Keeps track of pretoken counts. NOTE: the values
+        # in this counter NEVER changes, as they reflect the
+        # frequency of pretokens in corpus. What does change
+        # are the keys, which are tuple of bytes elements,
+        # where each bytes element correspond to some vocab,
+        # and as BPE evolves with merging, the freq's keys
+        # may change to different tuple of bytes, but the
+        # concatinated bytes together is ALWAYS the pretoken.
+        self._freq: dict[tuple[bytes, ...], int] \
             = collections.Counter()
         for pt in self._pretokens:
-            for l, r in itertools.pairwise(pt.encode()):
-                self._freq[(l, r)] += 1
+            key = tuple([
+                bytes([b])
+                for b in pt.encode()
+            ])
+            self._freq[key] += 1
+
+    def _pair_to_merge(self) -> tuple[bytes, bytes]:
+        '''Finds the most common bytes pair to merge.
+        
+        On tie, return the lexically largest candidate.
+        '''
+        pairs_counter = collections.Counter()
+        # reverse index to update freq
+        pair_to_pts = collections.defaultdict(set)
+        for pt, count in self._freq.items():
+            for l, r in itertools.pairwise(pt):
+                pairs_counter[(l, r)] += count
+                pair_to_pts[(l, r)].add(pt)
+        max_cnt = max(pairs_counter.values())
+        candidates = sorted([
+            pair
+            for pair, cnt in pairs_counter.items()
+            if cnt == max_cnt
+        ], key=lambda pair: pair[0] + pair[1])
+        candidate = candidates[-1]
+
+        # Update freq. For each pretoken subject to update
+        # (i.e. where the candidate is a substring) it runs a
+        # pairwise sliding window over the pretoken, to merge
+        # relevant vocab pairs.
+        for pt in pair_to_pts[candidate]:
+            pt_cnt = self._freq[pt]
+            del self._freq[pt]
+            new_pt = []
+            i = 0
+            while i < len(pt):
+                if pt[i:i+2] == candidate:
+                    new_pt.append(candidate[0] + candidate[1])
+                    i += 2
+                else:
+                    new_pt.append(pt[i])
+                    i += 1
+            new_pt = tuple(new_pt)
+            self._freq[new_pt] = pt_cnt
+        return candidate
 
     def _merge(self) -> None:
         '''Exercises 1 round of BPE merging.'''
-        ...
+        l, r = self._pair_to_merge()
+        new_vocab = l + r
+        # Update merge history.
+        self._merges.append((l, r))
+        # Update vocab.
+        self._vocabs[len(self._vocabs)] = new_vocab
 
     def train(self) -> \
         tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
--- cs336_basics/tokenizer/bpe_test.py
+++ cs336_basics/tokenizer/bpe_test.py
@@ -24,3 +24,47 @@ def test_init():
     })
     assert vocabs == expected_vocabs
     assert len(merges) == 0
+
+def test_example():
+    special_tokens = ['<|endoftext|>']
+    pretokens = [
+        'low',
+        'low',
+        'low',
+        'low',
+        'low',
+        'lower',
+        'lower',
+        'wildest',
+        'wildest',
+        'wildest',
+        'newest',
+        'newest',
+        'newest',
+        'newest',
+        'newest',
+        'newest',
+    ]
+    bpe = BytePairEncoder(
+        pretokens = pretokens,
+        # anything less than 256 + num special tokens is fine.
+        vocab_size = 263,
+        special_tokens = special_tokens,
+    )
+    vocabs, merges = bpe.train()
+    assert len(vocabs) == 263
+    assert vocabs[256] == b'<|endoftext|>'
+    assert vocabs[257] == b'st'
+    assert vocabs[258] == b'est'
+    assert vocabs[259] == b'ow'
+    assert vocabs[260] == b'low'
+    assert vocabs[261] == b'west'
+    assert vocabs[262] == b'ne'
+    assert merges == [
+        (b's', b't'),
+        (b'e', b'st'),
+        (b'o', b'w'),
+        (b'l', b'ow'),
+        (b'w', b'est'),
+        (b'n', b'e'),
+    ]
