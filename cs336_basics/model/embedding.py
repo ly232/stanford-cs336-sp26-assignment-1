fd48faa1f8a10964b9c7eff51a8ab23d50ffddd0
Lin Yang <yang7.cooper@gmail.com>
2026-07-15 21:43:48 +0800

    optimize the embedding layer by replacing the onehot vocab indices tensor materialization with direct tensor fancy indexing. this is another good use case of function composition based tensor indexing, where the indeces as a tensor can be directly feed into the embedding weights tensor as a lookup.

--- cs336_basics/model/embedding.py
+++ cs336_basics/model/embedding.py
@@ -21,17 +21,25 @@ class Embedding(torch.nn.Module):
 
     def forward(self, x: Float[torch.Tensor, 'batch seq'])\
         -> Float[torch.Tensor, 'batch seq d_model']:
-        # Onehot encode the input token ids.
-        onehot: Float[torch.Tensor, 'batch seq vocab_size']\
-            = torch.nn.functional.one_hot(
-                x, num_classes=self.vocab_size)
-        # print(f'''shapes and dtype:
-        # x: {x.shape}, {x.dtype}
-        # onehot: {onehot.shape}, {onehot.dtype}
-        # weights: {self.weights.shape}, {self.weights.dtype}
-        # ''')
-        return einsum(
-            # NOTE: einsum requires 2 tensors to share same dtype.
-            self.weights, onehot.to(self.weights.dtype),
-            'vocab_size d_model, batch seq vocab_size -> batch seq d_model'
-        )
+        # # Inefficient implementation (kept for learning purpose):
+        # # Onehot encode the input token ids.
+        # onehot: Float[torch.Tensor, 'batch seq vocab_size']\
+        #     = torch.nn.functional.one_hot(
+        #         x, num_classes=self.vocab_size)
+        # return einsum(
+        #     # NOTE: einsum requires 2 tensors to share same dtype.
+        #     self.weights, onehot.to(self.weights.dtype),
+        #     'vocab_size d_model, batch seq vocab_size -> batch seq d_model'
+        # )
+
+        # Directly apply tensor fancy indexing:
+        # * weights.shape is (vocab_size, d_model)
+        # * x.shape is (batch, seq)
+        # * weights[x] is thus (batch, seq, d_model)
+        #
+        # See tensor_indexing_memo.ipynb. weights[x] can
+        # be thought of as function composition, where
+        # the output of x (actual indices of each vocab
+        # position) can be directly feed into weights as
+        # query to find the corresponding embedding value.
+        return self.weights[x]
